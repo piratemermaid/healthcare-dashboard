4392ca42b284eec7136852d9ef6e98f34a9d5c50
Ariel <thepiratemermaid4@gmail.com>
2026-03-03 15:49:50 -0700

    set up tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { RouterProvider } from '@tanstack/react-router';
@@ -8,11 +10,16 @@ import { router } from './router';
 import { lightTheme } from '~/themes/light';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <RouterProvider router={router} />
-      <CssBaseline />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={lightTheme}>
+        <RouterProvider router={router} />
+        <CssBaseline />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+    </QueryClientProvider>
   </StrictMode>
 );
